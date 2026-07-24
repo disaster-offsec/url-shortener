579dc58997e24d8dc397084dca4e5269e8691827
Platon <platon.nickitin@yandex.ru>
2026-07-24 18:51:03 +0300

    Основная логика готова.

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
 )
 
 func main() {
@@ -20,16 +23,61 @@ func main() {
 	}
 }
 
+// на первое время сгодится
+func generateShortCode() string {
+	b := make([]byte, 4)
+	rand.Read(b)
+	return base64.URLEncoding.EncodeToString(b)[:6]
+}
+
+
+// Проверка метода необязательна
 func shortenHandler(storage *Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Заглушка
-		panic("shortenHandler not implemented yet")
+		var req struct {
+			URL string `json:"url"`
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		if req.URL == "" {
+			http.Error(w, "URL is required", http.StatusBadRequest)
+			return
+		}
+
+		code := generateShortCode()
+
+		for storage.Exists(code) {
+			code = generateShortCode()
+		}
+
+		storage.Save(code, req.URL)
+
+		shortURL := "http://localhost:8080/" + code
+		response := map[string]string{"short_url": shortURL}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
 	}
 }
 
 func redirectHandler(storage *Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Заглушка
-		panic("redirectHandler not implemented yet")
+		code := r.PathValue("short")
+		if code == "" {
+			http.NotFound(w, r)
+			return
+		}
+
+		originalURL, ok := storage.Get(code)
+
+		if !ok {
+			http.NotFound(w, r)
+			return
+		}
+
+		http.Redirect(w, r, originalURL, http.StatusFound)
 	}
 }
