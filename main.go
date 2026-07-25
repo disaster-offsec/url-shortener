6f9f8c19256f99d4de5a9c28900c485ea43a7ba0
Platon <platon.nickitin@yandex.ru>
2026-07-25 18:31:08 +0300

    quick fixes и более лучшее логирование

--- main.go
+++ main.go
@@ -17,18 +17,16 @@ import (
 func main() {
 	storage := NewStorage()
 
-	
 	port := os.Getenv("PORT")
 	if port == "" {port = "8080"}
 
 	baseURL := os.Getenv("BASE_URL")
 	if baseURL == "" {baseURL = "http://localhost:" + port}
 	
-	http.HandleFunc("POST /shorten", shortenHandler(baseURL, storage))
-	http.HandleFunc("GET /{short}", redirectHandler(storage))
+	http.HandleFunc("POST /shorten", loggingMiddleware(shortenHandler(baseURL, storage)))
+	http.HandleFunc("GET /{short}", loggingMiddleware(redirectHandler(storage)))
 
 
-	// Запускаем сервер на 8080 порт
 	log.Printf("Server starting on: %s", baseURL)
 
 	server := &http.Server{Addr: ":" + port, Handler: nil}
@@ -53,6 +51,15 @@ func main() {
 	log.Println("Server exited properly")
 }
 
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("Started %s %s", r.Method, r.URL.Path)
+		next(w, r)
+		log.Printf("Completed %s %s in %v", r.Method, r.URL.Path, time.Since(start))
+	}
+}
+
 func isValidURL(raw string) bool {
 	parsed, err := url.Parse(raw)
 	return err == nil && (parsed.Scheme == "http" || parsed.Scheme == "https")
@@ -66,18 +73,20 @@ func generateShortCode() string {
 }
 
 // Проверка метода необязательна
-func shortenHandler(string baseURL, storage *Storage) http.HandlerFunc {
+func shortenHandler(baseURL string, storage *Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req struct {
 			URL string `json:"url"`
 		}
 
 		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			log.Printf("Invalid JSON: %v", err)
 			http.Error(w, "Invalid JSON", http.StatusBadRequest)
 			return
 		}
 
 		if !isValidURL(req.URL) {
+			log.Printf("Invalid URL scheme (must be http or https)")
 			http.Error(w, "Invalid URL scheme (must be http or https)", http.StatusBadRequest)
 			return
 		}	
@@ -109,6 +118,7 @@ func redirectHandler(storage *Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		code := r.PathValue("short")
 		if code == "" {
+			log.Printf("Short code not found %s", code) // да там "", но тем не менее
 			http.NotFound(w, r)
 			return
 		}
@@ -117,6 +127,7 @@ func redirectHandler(storage *Storage) http.HandlerFunc {
 		originalURL, ok := storage.GetByCode(code)
 
 		if !ok {
+			log.Printf("Short code not found %s", code)
 			http.NotFound(w, r)
 			return
 		}
